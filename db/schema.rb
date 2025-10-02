5e613e8369c7837c7e7b168687c424b57436bbc1
yungProg <122945393+yungProg@users.noreply.github.com>
2025-10-02 21:22:01 +0000

    create Post model and persisted first post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_02_172701) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_02_205843) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
