ee54c79b62afbb06666d518959f944a5451c454b
yungProg <122945393+yungProg@users.noreply.github.com>
2025-10-02 18:02:27 +0000

    validates users columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :display_name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\w/, message: "only allows letters" }
 end
