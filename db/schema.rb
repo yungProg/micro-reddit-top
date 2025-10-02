157c02400a06a7ec6362165bee5296a409e925c0
yungProg <122945393+yungProg@users.noreply.github.com>
2025-10-02 21:25:48 +0000

    create Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_02_205843) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_02_212455) do
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
